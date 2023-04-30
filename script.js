62d17704469981f58f0cec843c00e2aa44cbb2b2
Zgibbs58 <zacharywgibbs@gmail.com>
2023-04-30 16:47:57 -0500

    added html, js, and assets file. Created html code for button and added javascript code to start making game.

---
+++ script.js
@@ -0,0 +1,42 @@
+var playBtn = document.querySelector("#playBtn");
+
+function rockPaperScissors() {
+  var playersChoice = prompt(
+    "Choose between rock, paper, or scissors. Type R, P, or S below."
+  );
+  if (playersChoice === "R") {
+    var playersChoice = 0;
+  } else if (playersChoice === "P") {
+    var playersChoice = 1;
+  } else if (playersChoice === "S") {
+    var playersChoice = 2;
+  }
+
+  var opponentChoices = ["R", "P", "S"];
+  var opponentChoice = "";
+  for (let index = 0; index < opponentChoices.length; index++) {
+    var randomChoice = opponentChoices.charAt(
+      Math.floor(Math.random() * opponentChoices.length)
+    );
+    opponentChoice += randomChoice;
+  }
+
+  if (opponentChoice() === playersChoice) {
+    alert("It was a tie, try again!");
+    return rockPaperScissors();
+  } else if (opponentChoice() === 0 && playersChoice === 1) {
+    alert("You chose paper, opponent chose rock. You win!");
+  } else if (opponentChoice() === 0 && playersChoice === 2) {
+    alert("You chose scissors, opponent chose rock. You lose!");
+  } else if (opponentChoice() === 1 && playersChoice === 0) {
+    alert("You chose rock, opponent chose paper. You lose!");
+  } else if (opponentChoice() === 1 && playersChoice === 2) {
+    alert("You chose scissors, opponent chose paper. You win!");
+  } else if (opponentChoice() === 2 && playersChoice === 0) {
+    alert("You chose rock, opponent chose scissors. You win!");
+  } else if (opponentChoice() === 2 && playersChoice === 1) {
+    alert("You chose paper, opponent chose scissors. You lose!");
+  }
+}
+
+playBtn.addEventListener("click", rockPaperScissors);
