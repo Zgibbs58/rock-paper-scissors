3aa73e946c9f5bb4d058d4bd2f9ba579e9eff01a
Zgibbs58 <zacharywgibbs@gmail.com>
2023-04-30 18:16:06 -0500

    added variables to capture score for html and add 1 to the wins, losses, or ties.

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 var playBtn = document.querySelector("#playBtn");
+var updateWin = document.querySelector(".playerWins");
+var updateLosses = document.querySelector(".playerLosses");
+var updateTies = document.querySelector(".ties");
+
+// console.log(parseInt(updateWin.innerHTML) + 1);
 
 function rockPaperScissors() {
   var playersChoice = prompt(
@@ -16,23 +21,33 @@ function rockPaperScissors() {
   var opponentChoice = opponentChoices.charAt(
     Math.floor(Math.random() * opponentChoices.length)
   );
-  console.log(opponentChoice);
 
   if (opponentChoice === playersChoice) {
+    updateTies.innerHTML = parseInt(updateTies.innerHTML) + 1;
     alert("It was a tie!");
   } else if (playersChoice === "P" && opponentChoice === "R") {
+    updateWin.innerHTML = parseInt(updateWin.innerHTML) + 1;
     alert("You chose paper, opponent chose rock. You win!");
   } else if (playersChoice === "S" && opponentChoice === "R") {
+    updateLosses.innerHTML = parseInt(updateLosses.innerHTML) + 1;
     alert("You chose scissors, opponent chose rock. You lose!");
   } else if (playersChoice === "R" && opponentChoice === "P") {
+    updateLosses.innerHTML = parseInt(updateLosses.innerHTML) + 1;
     alert("You chose rock, opponent chose paper. You lose!");
   } else if (playersChoice === "S" && opponentChoice === "P") {
+    updateWin.innerHTML = parseInt(updateWin.innerHTML) + 1;
     alert("You chose scissors, opponent chose paper. You win!");
   } else if (playersChoice === "R" && opponentChoice === "S") {
+    updateWin.innerHTML = parseInt(updateWin.innerHTML) + 1;
     alert("You chose rock, opponent chose scissors. You win!");
   } else if (playersChoice === "P" && opponentChoice === "S") {
+    updateLosses.innerHTML = parseInt(updateLosses.innerHTML) + 1;
     alert("You chose paper, opponent chose scissors. You lose!");
   }
+
+  //   function scoreBoard(currentValue) {
+  //     currentValue.innerHTML;
+  //   }
 
   var playAgain = confirm(
     "If you want to play again, hit okay. If you don't, hit cancel."
